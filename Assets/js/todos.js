f9b0a42c42e7499bd07758a8c7e2d382896345b1
NimishMetrani <65410339+NimishMetrani@users.noreply.github.com>
2020-05-29 17:24:22 -0400

    Adding asset

---
+++ Assets/js/todos.js
@@ -0,0 +1,23 @@
+//check off specific todos by clicking
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("completed");
+});
+
+//Click on X to delete ToDo.
+$("ul").on("click","span",function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+  event.stopPropagation();
+})
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var toDo = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span class = 'toggle'><i class='fas fa-trash-alt'></i></span> " + toDo + "</li>");
+    }
+})
+$("#hide").click(function(){
+    $("input[type='text']").fadeToggle();
+
+});
